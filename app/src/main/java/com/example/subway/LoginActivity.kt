499cb0539ae95ced0685e087535d9599232695e7
irissel <shp10015@naver.com>
2023-11-17 16:09:17 +0900

    로그인 테스트

---
+++ app/src/main/java/com/example/subway/LoginActivity.kt
@@ -0,0 +1,14 @@
+package com.example.subway
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+
+    }
+}
